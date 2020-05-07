5f3dff91d1f7f14ed0f1e6c16e126d80022c8312
Prudviraj Boddula <59728176+boddpru@users.noreply.github.com>
2020-05-07 09:59:38 +0530

    latest

---
+++ Eureka/hydragui/dist/assets/data/hydraulic-calculation-data.ts
@@ -0,0 +1,266 @@
+export const HydraulicCalculationData = {
+  'lang': 'de',
+  'hydraulic_calculation_data': [
+  {
+    'id': 'valve_medium',
+    'type': 'INT',
+    'title': 'Betriebstemperatur °C'
+  },
+  {
+    'id': 'op_flow',
+    'type': 'INT',
+    'title': 'options',
+    'label': 'm3/h',
+    'options': [
+      {
+        'id': 'trgt_dschrg_flow',
+        'title': 'Förderstrom Q'
+      },
+      {
+        'id': 'trgt_mass_flow_rate',
+        'title': 'Massenstrom'
+      }
+    ]
+  },
+  {
+    'id': 'op_head',
+    'type': 'INT',
+    'title': 'options',
+    'options': [
+      {
+        'id': 'trgt_dschrg_head',
+        'title': 'Förderhöhe H'
+      },
+      {
+        'id': 'trgt_dschrg_prssr',
+        'title': 'Enddruck'
+      }
+    ]
+  },
+  {
+    'id': 'gdtc_head',
+    'type': 'INT',
+    'title': 'geo. Förderhöhe Hgeo'
+  },
+  {
+    'id': 'op_npsh',
+    'type': 'INT',
+    'title': 'options',
+    'options': [
+      {
+        'id': 'npsh_avlbl',
+        'title': 'NPSH vorhanden'
+      },
+      {
+        'id': 'prs_inlt_spc',
+        'title': 'min. Zulaufdruck'
+      }
+    ]
+  },
+  {
+    'id': 'dschrg_head_rsrv_min_spc',
+    'type': 'INT',
+    'title': 'Förderhöhenreserve',
+    'options': [
+      {
+        'id': '0',
+        'title': '+/-0 %'
+      },
+      {
+        'id': '1',
+        'title': '+1 %'
+      },
+      {
+        'id': '2',
+        'title': '+2 %'
+      },
+      {
+        'id': '3',
+        'title': '+3 %'
+      },
+      {
+        'id': '4',
+        'title': '+4 %'
+      },
+      {
+        'id': '5',
+        'title': '+5 %'
+      },
+      {
+        'id': '-1',
+        'title': '-1 %'
+      },
+      {
+        'id': '-2',
+        'title': '-2 %'
+      },
+      {
+        'id': '-3',
+        'title': '-3 %'
+      },
+      {
+        'id': '-4',
+        'title': '-4 %'
+      },
+      {
+        'id': '-5',
+        'title': '-5 %'
+      }
+    ]
+  },
+  {
+    'id': 'crv_slp_min_spc',
+    'type': 'INT',
+    'title': 'erford. Foerderhöhenanstieg',
+    'options': [
+      {
+        'id': '0',
+        'title': '+/-0 %'
+      },
+      {
+        'id': '1',
+        'title': '+1 %'
+      },
+      {
+        'id': '2',
+        'title': '+2 %'
+      },
+      {
+        'id': '3',
+        'title': '+3 %'
+      },
+      {
+        'id': '4',
+        'title': '+4 %'
+      },
+      {
+        'id': '5',
+        'title': '+5 %'
+      },
+      {
+        'id': '-1',
+        'title': '-1 %'
+      },
+      {
+        'id': '-2',
+        'title': '-2 %'
+      },
+      {
+        'id': '-3',
+        'title': '-3 %'
+      },
+      {
+        'id': '-4',
+        'title': '-4 %'
+      },
+      {
+        'id': '-5',
+        'title': '-5 %'
+      }
+    ]
+  },
+  {
+    'id': 'rt_q_qopt_min_spc',
+    'type': 'INT',
+    'title': 'Ratio Q/Qopt min.',
+    'options': [
+      {
+        'id': '0',
+        'title': '+/-0 %'
+      },
+      {
+        'id': '1',
+        'title': '+1 %'
+      },
+      {
+        'id': '2',
+        'title': '+2 %'
+      },
+      {
+        'id': '3',
+        'title': '+3 %'
+      },
+      {
+        'id': '4',
+        'title': '+4 %'
+      },
+      {
+        'id': '5',
+        'title': '+5 %'
+      },
+      {
+        'id': '-1',
+        'title': '-1 %'
+      },
+      {
+        'id': '-2',
+        'title': '-2 %'
+      },
+      {
+        'id': '-3',
+        'title': '-3 %'
+      },
+      {
+        'id': '-4',
+        'title': '-4 %'
+      },
+      {
+        'id': '-5',
+        'title': '-5 %'
+      }
+    ]
+  },
+  {
+    'id': 'rt_q_qopt_max_spc',
+    'type': 'INT',
+    'title': 'Ratio Q/Qopt max.',
+    'options': [
+      {
+        'id': '0',
+        'title': '+/-0 %'
+      },
+      {
+        'id': '1',
+        'title': '+1 %'
+      },
+      {
+        'id': '2',
+        'title': '+2 %'
+      },
+      {
+        'id': '3',
+        'title': '+3 %'
+      },
+      {
+        'id': '4',
+        'title': '+4 %'
+      },
+      {
+        'id': '5',
+        'title': '+5 %'
+      },
+      {
+        'id': '-1',
+        'title': '-1 %'
+      },
+      {
+        'id': '-2',
+        'title': '-2 %'
+      },
+      {
+        'id': '-3',
+        'title': '-3 %'
+      },
+      {
+        'id': '-4',
+        'title': '-4 %'
+      },
+      {
+        'id': '-5',
+        'title': '-5 %'
+      }
+    ]
+  }
+]
+};
+
---
+++ Eureka/searchservice/externalConfig/readme.md
@@ -0,0 +1,16 @@
+# External config directory for VNBASE1C
+
+@author:patazol
+
+
+
+This is the directory where 
+
+Eureka configuration files for VNBASE1C do reside in.
+
+
+
+
+
+
+
